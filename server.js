76b33469e426ef3b7fcd6633b087cd95508a01e4
ondalk8s <hiondal@gmail.com>
2019-09-20 12:43:28 +0900

    ffffffff

--- server.js
+++ server.js
@@ -1,53 +1,57 @@
-var express    = require('express');
-var app        = express();
-var path       = require('path');
-var mongoose   = require('mongoose');
+var express = require('express');
+var app = express();
+var path = require('path');
+var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 global.__BASEDIR = __dirname + '/';
 
 // Database
-const connStr = process.env.MONGO_DB_LOGIN_API || "mongodb://admin:passw0rd@169.56.164.245:27017/users";
+const connStr = process.env.MONGO_DB_LOGIN_API
+		|| "mongodb://admin:passw0rd@169.56.164.245:27017/users";
 mongoose.Promise = global.Promise;
 const options = {
-	autoIndex: false, // Don't build indexes
-	reconnectTries: 10, // Never stop trying to reconnect
-	reconnectInterval: 500, // Reconnect every 500ms
-	poolSize: 10, // Maintain up to 10 socket connections
-	// If not connected, return errors immediately rather than waiting for reconnect
-	bufferMaxEntries: 0,
-	connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
-	socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-	family: 4, // Use IPv4, skip trying IPv6
-	useMongoClient: true
+	autoIndex : false, // Don't build indexes
+	reconnectTries : 10, // Never stop trying to reconnect
+	reconnectInterval : 500, // Reconnect every 500ms
+	poolSize : 10, // Maintain up to 10 socket connections
+	// If not connected, return errors immediately rather than waiting for
+	// reconnect
+	bufferMaxEntries : 0,
+	connectTimeoutMS : 10000, // Give up initial connection after 10 seconds
+	socketTimeoutMS : 45000, // Close sockets after 45 seconds of inactivity
+	family : 4, // Use IPv4, skip trying IPv6
+	useMongoClient : true
 };
-console.log("->"+connStr);
+console.log("->" + connStr);
 mongoose.connect(connStr, options);
 var db = mongoose.connection;
 
-db.once('openUri', function () {
-   console.log('DB connected!');
+db.once('openUri', function() {
+	console.log('DB connected!');
 });
 
-db.on('error', function (err) {
-  console.log('DB ERROR:', err);
+db.on('error', function(err) {
+	console.log('DB ERROR:', err);
 });
 
 // Middlewares
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'content-type, x-access-token');
-  next();
+app.use(bodyParser.urlencoded({
+	extended : true
+}));
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	res.header('Access-Control-Allow-Headers', 'content-type, x-access-token');
+	next();
 });
 
 // API
-app.use('/api/users', require(path.join(__BASEDIR,'api/users')));
+app.use('/api/users', require(path.join(__BASEDIR, 'api/users')));
 app.use('/api/auth', require(path.join(__BASEDIR, '/api/auth')));
 
 // Server
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
-  console.log('listening on port:' + port);
+app.listen(port, function() {
+	console.log('listening on port:' + port);
 });
--- util.js
+++ util.js
@@ -2,57 +2,66 @@ var jwt = require('jsonwebtoken');
 
 var util = {};
 
-const  JWT_SECRET = process.env.JWT_SECRET || "MySecretKey";
+const JWT_SECRET = process.env.JWT_SECRET || "MySecretKey";
 util.JWT_SECRET = JWT_SECRET;
 
-util.successTrue = function(data){
-  return {
-    success:true,
-    message:null,
-    errors:null,
-    data:data
-  };
+util.successTrue = function(data) {
+	return {
+		success : true,
+		message : null,
+		errors : null,
+		data : data
+	};
 };
 
-util.successFalse = function(err, message){
-  if(!err&&!message) message = 'data not found';
-  return {
-    success:false,
-    message:message,
-    errors:(err)? util.parseError(err): null,
-    data:null
-  };
+util.successFalse = function(err, message) {
+	if (!err && !message)
+		message = 'data not found';
+	return {
+		success : false,
+		message : message,
+		errors : (err) ? util.parseError(err) : null,
+		data : null
+	};
 };
 
-util.parseError = function(errors){
-  var parsed = {};
-  if(errors.name == 'ValidationError'){
-    for(var name in errors.errors){
-      var validationError = errors.errors[name];
-      parsed[name] = { message:validationError.message };
-    }
-  } else if(errors.code == '11000' && errors.errmsg.indexOf('username') > 0) {
-    parsed.username = { message:'This username already exists!' };
-  } else {
-    parsed.unhandled = errors;
-  }
-  return parsed;
+util.parseError = function(errors) {
+	var parsed = {};
+	if (errors.name == 'ValidationError') {
+		for ( var name in errors.errors) {
+			var validationError = errors.errors[name];
+			parsed[name] = {
+				message : validationError.message
+			};
+		}
+	} else if (errors.code == '11000' && errors.errmsg.indexOf('username') > 0) {
+		parsed.username = {
+			message : 'This username already exists!'
+		};
+	} else {
+		parsed.unhandled = errors;
+	}
+	return parsed;
 };
-
 
 // middlewares
-util.isLoggedin = function(req,res,next){
-  var token = req.headers['x-access-token'];
-  if (!token) return res.json(util.successFalse(null,'token is required!'));
-  else {
-    jwt.verify(token, JWT_SECRET, function(err, decoded) {
-      if(err) return res.json(util.successFalse(err));
-      else{
-        req.decoded = decoded;
-        next();
-      }
-    });
-  }
+util.isLoggedin = function(req, res, next) {
+	var token = req.headers['x-access-token'];
+	if (!token)
+		return res.json(util.successFalse(null, 'token is required!'));
+	else {
+		console.log("## verify target token ==>" + token);
+		jwt.verify(token, JWT_SECRET, function(err, decoded) {
+			if (err) {
+				console.log("error=>" + err.message);
+				return res.json(util.successFalse(err));
+			} else {
+				console.log("success to verify => " + decoded);
+				req.decoded = decoded;
+				next();
+			}
+		});
+	}
 };
 
 module.exports = util;
